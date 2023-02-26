f3340d437b76925d449a89881b4326efa821389c
bc-kim <bc.kim@worksmobile.com>
2023-02-26 20:40:20 +0900

    chore: 개발 환경 세팅

---
+++ src/main/java/com/poolc/springproject/poolcreborn/PoolcrebornApplication.java
@@ -0,0 +1,13 @@
+package com.poolc.springproject.poolcreborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolcrebornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolcrebornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poolc/springproject/poolcreborn/PoolcrebornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poolc.springproject.poolcreborn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolcrebornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
