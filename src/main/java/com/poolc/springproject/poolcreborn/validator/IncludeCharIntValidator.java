cc0caddb7de0485cbe26a3a4b4e0bdc10450043f
jinny-codes <becooq81@gmail.com>
2023-03-04 00:09:23 +0900

    feat: added validation implementation for including characters and integers

--- src/main/java/com/poolc/springproject/poolcreborn/validator/IncludeCharIntValidator.java
+++ src/main/java/com/poolc/springproject/poolcreborn/validator/IncludeCharIntValidator.java
@@ -1,4 +1,26 @@
 package com.poolc.springproject.poolcreborn.validator;
 
-public class IncludeCharIntValidator {
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class IncludeCharIntValidator implements ConstraintValidator<IncludeCharInt, String> {
+    char[] chars;
+
+    @Override
+    public void initialize(IncludeCharInt constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        chars = value.toCharArray();
+        for (char c : chars) {
+            if (Character.isLetter(c) || Character.isDigit(c)) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
