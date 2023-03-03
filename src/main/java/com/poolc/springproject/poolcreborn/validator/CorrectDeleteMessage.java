13b59ee8a1b4f7d4cc83df99f7d9481ace684042
jinny-codes <becooq81@gmail.com>
2023-03-04 00:11:53 +0900

    feat: added validation interface for ensuring the delete message is correct

---
+++ src/main/java/com/poolc/springproject/poolcreborn/validator/CorrectDeleteMessage.java
@@ -0,0 +1,21 @@
+package com.poolc.springproject.poolcreborn.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target( { ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = CorrectDeleteMessageValidator.class)
+public @interface CorrectDeleteMessage {
+
+    //error message
+    public String message() default "The message is incorrect";
+    //represents group of constraints
+    public Class<?>[] groups() default {};
+    //represents additional information about annotation
+    public Class<? extends Payload>[] payload() default {};
+
+
+}
