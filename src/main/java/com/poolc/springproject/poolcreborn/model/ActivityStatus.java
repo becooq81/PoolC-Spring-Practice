5539d2ae2cdb16858ac11e91186071239bcb709b
jinny-codes <becooq81@gmail.com>
2023-03-04 00:08:05 +0900

    feat: added Activity Status enum

--- src/main/java/com/poolc/springproject/poolcreborn/model/ActivityStatus.java
+++ src/main/java/com/poolc/springproject/poolcreborn/model/ActivityStatus.java
@@ -1,4 +1,20 @@
 package com.poolc.springproject.poolcreborn.model;
 
 public enum ActivityStatus {
+
+    DEFAULT("일반 회원"),
+    STOP("한 학기 비활동"),
+    GRADUATED("졸업회원"),
+    COMPLETED("수료회원");
+
+    private final String value;
+
+    ActivityStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
 }
