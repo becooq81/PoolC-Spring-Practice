18382be7cd4574627e7a0a0fa91a555c9f053d31
jinny-codes <becooq81@gmail.com>
2023-03-04 00:07:37 +0900

    feat: added role class

--- src/main/java/com/poolc/springproject/poolcreborn/model/Role.java
+++ src/main/java/com/poolc/springproject/poolcreborn/model/Role.java
@@ -1,4 +1,22 @@
 package com.poolc.springproject.poolcreborn.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+
 }
