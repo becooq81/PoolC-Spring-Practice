b03673172aa3827b8f79ebc0ebfbfd2c810a7ecc
jinny-codes <becooq81@gmail.com>
2023-03-04 00:10:10 +0900

    feat: added role repository

--- src/main/java/com/poolc/springproject/poolcreborn/repository/RoleRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.poolc.springproject.poolcreborn.repository;
 
-public class RoleRepository {
+import com.poolc.springproject.poolcreborn.model.ERole;
+import com.poolc.springproject.poolcreborn.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRole(ERole role);
 }
