598ad73cf7e6a78345f088a5aff8ccaaf2d2b2e0
jinny-codes <becooq81@gmail.com>
2023-03-04 15:31:46 +0900

    feat: added validation interface to ensure the input student id string is an integer

--- src/main/java/com/poolc/springproject/poolcreborn/validator/IsInteger.java
+++ src/main/java/com/poolc/springproject/poolcreborn/validator/IsInteger.java
@@ -1,2 +1,17 @@
-package com.poolc.springproject.poolcreborn.validator;public class IsInteger {
+package com.poolc.springproject.poolcreborn.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target( { ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = IsIntegerValidator.class)
+public @interface IsInteger {
+
+    String message() default "The student id must be composed of integers.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
 }
