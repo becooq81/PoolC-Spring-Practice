e1051e7d231b38fac6cbc1b1a81232f73d898f79
jinny-codes <becooq81@gmail.com>
2023-03-11 19:35:37 +0900

    feat(AdminController): added method to modify user roles

--- src/main/java/com/poolc/springproject/poolcreborn/controller/AdminController.java
+++ src/main/java/com/poolc/springproject/poolcreborn/controller/AdminController.java
@@ -1,2 +1,37 @@
-package com.poolc.springproject.poolcreborn.controller;public class AdminController {
+package com.poolc.springproject.poolcreborn.controller;
+
+import com.poolc.springproject.poolcreborn.payload.request.UserVo;
+import com.poolc.springproject.poolcreborn.repository.UserRepository;
+import com.poolc.springproject.poolcreborn.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final UserService userService;
+
+    @PatchMapping("/add")
+    public ResponseEntity<?> addAdmins(@Valid @RequestBody List<UserVo> userVos) {
+        for (UserVo userVo : userVos) {
+            String username = userVo.getUsername();
+            if (userVo.isAdmin()) {
+                userService.addAdminRole(username);
+            }
+            if (userVo.isMember()) {
+                userService.addMemberRole(username);
+            }
+        }
+        return ResponseEntity.ok("Roles have been modified successfully.");
+    }
 }
