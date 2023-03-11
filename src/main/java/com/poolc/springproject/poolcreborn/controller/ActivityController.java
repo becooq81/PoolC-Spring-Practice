873a32ce8af3fa942c15e30de2aebcb9695ee4a8
jinny-codes <becooq81@gmail.com>
2023-03-11 21:23:54 +0900

    feat(ActivityController): added update activity method

--- src/main/java/com/poolc/springproject/poolcreborn/controller/ActivityController.java
+++ src/main/java/com/poolc/springproject/poolcreborn/controller/ActivityController.java
@@ -2,16 +2,17 @@ package com.poolc.springproject.poolcreborn.controller;
 
 import com.poolc.springproject.poolcreborn.model.Activity;
 import com.poolc.springproject.poolcreborn.payload.request.ActivityRequest;
+import com.poolc.springproject.poolcreborn.payload.request.ActivityUpdateRequest;
 import com.poolc.springproject.poolcreborn.repository.ActivityRepository;
 import com.poolc.springproject.poolcreborn.service.ActivityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import static com.poolc.springproject.poolcreborn.security.SecurityUtil.getLoginUsername;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,12 +23,19 @@ public class ActivityController {
 
     @PostMapping("/new")
     public ResponseEntity<?> registerActivity(@RequestBody @Valid ActivityRequest activityRequest) {
-        activityService.saveActivity(activityRequest);
+        String username = getLoginUsername();
+        activityService.saveActivity(activityRequest, username);
         return ResponseEntity.ok("Activity registered successfully!");
     }
 
-    @PatchMapping("/edit")
-    public ResponseEntity<?> updateActivity(@RequestBody @Valid ActivityUpdateRequest activityUpdateRequest) {
-
+    @PatchMapping("/{id}/edit")
+    public ResponseEntity<?> updateActivity(@PathVariable("id") Long currentActivityId, @RequestBody @Valid ActivityUpdateRequest activityUpdateRequest) {
+        String username = getLoginUsername();
+        Activity activity = activityRepository.findById(currentActivityId).get();
+        if (activity.getUser().getUsername().equals(username)) {
+            activityService.updateActivity(activityUpdateRequest, currentActivityId);
+            return ResponseEntity.ok("Activity is updated successfully!");
+        }
+        return ResponseEntity.ok("Activity can be only updated by owner.");
     }
 }
