96831154b3772623e750658106531e5480443494
jinny-codes <becooq81@gmail.com>
2023-03-11 20:51:28 +0900

    feat(ActivityService): added save method

--- src/main/java/com/poolc/springproject/poolcreborn/service/ActivityService.java
+++ src/main/java/com/poolc/springproject/poolcreborn/service/ActivityService.java
@@ -1,2 +1,22 @@
-package com.poolc.springproject.poolcreborn.service;public class ActivityService {
+package com.poolc.springproject.poolcreborn.service;
+
+import com.poolc.springproject.poolcreborn.model.Activity;
+import com.poolc.springproject.poolcreborn.payload.request.ActivityRequest;
+import com.poolc.springproject.poolcreborn.repository.ActivityRepository;
+import com.poolc.springproject.poolcreborn.util.ActivityMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+    private final ActivityMapper activityMapper;
+
+    public void saveActivity(ActivityRequest activityRequest) {
+        Activity activity = new Activity();
+        activityMapper.buildActivityFromRequest(activityRequest, activity);
+        activityRepository.save(activity);
+    }
 }
