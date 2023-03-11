f22b3d2bdd4a9a7fc06d2bc9f3155defa1fb1381
jinny-codes <becooq81@gmail.com>
2023-03-11 20:37:44 +0900

    feat: added ActivityRepository

--- src/main/java/com/poolc/springproject/poolcreborn/repository/ActivityRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/ActivityRepository.java
@@ -1,2 +1,13 @@
-package com.poolc.springproject.poolcreborn.repository;public interface ActivityRepository {
+package com.poolc.springproject.poolcreborn.repository;
+
+import com.poolc.springproject.poolcreborn.model.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    List<Activity> findByUsername(String username);
+
 }
