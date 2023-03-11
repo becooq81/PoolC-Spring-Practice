33e8ccd8d3e451ab1b5caeddecef39e085a6c96d
jinny-codes <becooq81@gmail.com>
2023-03-11 21:02:15 +0900

    feat: added ActivityUpdateRequest payload

--- src/main/java/com/poolc/springproject/poolcreborn/payload/request/ActivityUpdateRequest.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/request/ActivityUpdateRequest.java
@@ -1,2 +1,47 @@
-package com.poolc.springproject.poolcreborn.payload.request;public class ActivityUpdateRequest {
+package com.poolc.springproject.poolcreborn.payload.request;
+
+import com.poolc.springproject.poolcreborn.model.ActivityType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ToString
+@Builder(toBuilder = true)
+public class ActivityUpdateRequest {
+
+    private String title;
+
+    private LocalDate startDate;
+
+    @Enumerated(EnumType.STRING)
+    private ActivityType activityType;
+
+    private int capacity;
+
+    private String schedule;
+
+    private int hours;
+
+    private List<String> tags = new ArrayList<>();
+
+    private String plan;
+
+    public ActivityUpdateRequest(String title, LocalDate startDate, ActivityType activityType, int capacity, String schedule, int hours, List<String> tags, String plan) {
+        this.title = title;
+        this.startDate = startDate;
+        this.activityType = activityType;
+        this.capacity = capacity;
+        this.schedule = schedule;
+        this.hours = hours;
+        this.tags.addAll(tags);
+        this.plan = plan;
+    }
+    public ActivityUpdateRequest() {}
 }
