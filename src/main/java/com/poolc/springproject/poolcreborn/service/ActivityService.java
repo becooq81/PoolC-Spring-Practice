3096f6d9a157213b808da4caa07c6434f27a6562
jinny-codes <becooq81@gmail.com>
2023-03-11 21:07:56 +0900

    feat(ActivityService): added update method

--- src/main/java/com/poolc/springproject/poolcreborn/service/ActivityService.java
+++ src/main/java/com/poolc/springproject/poolcreborn/service/ActivityService.java
@@ -2,6 +2,7 @@ package com.poolc.springproject.poolcreborn.service;
 
 import com.poolc.springproject.poolcreborn.model.Activity;
 import com.poolc.springproject.poolcreborn.payload.request.ActivityRequest;
+import com.poolc.springproject.poolcreborn.payload.request.ActivityUpdateRequest;
 import com.poolc.springproject.poolcreborn.repository.ActivityRepository;
 import com.poolc.springproject.poolcreborn.util.ActivityMapper;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,12 @@ public class ActivityService {
     public void saveActivity(ActivityRequest activityRequest) {
         Activity activity = new Activity();
         activityMapper.buildActivityFromRequest(activityRequest, activity);
+        activityRepository.save(activity);
+    }
+
+    public void updateActivity(ActivityUpdateRequest activityUpdateRequest, Long currentActivityId) {
+        Activity activity = activityRepository.findById(currentActivityId).get();
+        activityMapper.updateActivityInfoFromRequest(activityUpdateRequest, activity);
         activityRepository.save(activity);
     }
 }
