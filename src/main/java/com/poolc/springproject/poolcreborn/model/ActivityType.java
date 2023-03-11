e8745cd6d6d766561c5cee835c15926723b0d8f6
jinny-codes <becooq81@gmail.com>
2023-03-11 19:58:07 +0900

    feat: added enum ActivityType

---
+++ src/main/java/com/poolc/springproject/poolcreborn/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.poolc.springproject.poolcreborn.model;
+
+public enum ActivityType {
+    SEMINAR("세미나"),
+    STUDY("스터디");
+
+    private final String value;
+    ActivityType(String value) {
+        this.value = value;
+    }
+    public String getValue() {
+        return this.value;
+    }
+}
