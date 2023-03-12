ce84ad227ed441dd8084ff55f1dd7da1b59618bd
jinny-codes <becooq81@gmail.com>
2023-03-12 18:22:55 +0900

    feat(AdminController): getter method

--- src/main/java/com/poolc/springproject/poolcreborn/controller/AdminController.java
+++ src/main/java/com/poolc/springproject/poolcreborn/controller/AdminController.java
@@ -1,13 +1,12 @@
 package com.poolc.springproject.poolcreborn.controller;
 
 import com.poolc.springproject.poolcreborn.payload.request.user.UserVo;
+import com.poolc.springproject.poolcreborn.payload.response.UserDto;
 import com.poolc.springproject.poolcreborn.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -18,6 +17,12 @@ import java.util.List;
 public class AdminController {
 
     private final UserService userService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> admin() {
+        List<UserDto> userDtos = userService.findAllUsers();
+        return new ResponseEntity<>(userDtos, HttpStatus.OK);
+    }
 
     @PatchMapping("/add")
     public ResponseEntity<?> addRoles(@Valid @RequestBody List<UserVo> userVos) {
