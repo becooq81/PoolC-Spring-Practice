cccfd31f4981bcf22ace518db67a73c6e77a4e4b
jinny-codes <becooq81@gmail.com>
2023-03-12 18:04:02 +0900

    fix(ActivityRepository): fixed find by user method

--- src/main/java/com/poolc/springproject/poolcreborn/repository/ActivityRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/ActivityRepository.java
@@ -1,14 +1,16 @@
 package com.poolc.springproject.poolcreborn.repository;
 
 import com.poolc.springproject.poolcreborn.model.Activity;
+import com.poolc.springproject.poolcreborn.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
-    List<Activity> findByUsername(String username);
+    List<Activity> findByUser(User user);
     Optional<Activity> findById(Long id);
 
 }
