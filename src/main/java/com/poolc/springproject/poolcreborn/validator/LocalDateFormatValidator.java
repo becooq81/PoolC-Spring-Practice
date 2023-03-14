1b08e9a14db286f3300774dcbac03bce5ed0e0ff
jinny-codes <becooq81@gmail.com>
2023-03-15 00:06:51 +0900

    feat: implemented local date format validator

--- src/main/java/com/poolc/springproject/poolcreborn/validator/LocalDateFormatValidator.java
+++ src/main/java/com/poolc/springproject/poolcreborn/validator/LocalDateFormatValidator.java
@@ -1,2 +1,23 @@
-package com.poolc.springproject.poolcreborn.validator;public class LocalDateFormatValidator {
+package com.poolc.springproject.poolcreborn.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+
+public class LocalDateFormatValidator implements ConstraintValidator<LocalDateFormat, String> {
+    @Override
+    public void initialize(LocalDateFormat constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {return true;}
+        try {
+            LocalDate localDate = LocalDate.parse(value);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 }
