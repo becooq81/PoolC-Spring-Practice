c3d4241ede4fd17c84f6fe689348833f023607c4
2020190143 <heejin.kang@yonsei.ac.kr>
2023-03-16 17:46:23 +0900

    feat: implemented Day enum

---
+++ src/main/java/com/poolc/springproject/poolcreborn/model/Day.java
@@ -0,0 +1,23 @@
+package com.poolc.springproject.poolcreborn.model;
+
+public enum Day {
+
+    MONDAY("매주 월요일"),
+    TUESDAY("매주 화요일"),
+    WEDNESDAY("매주 수요일"),
+    THURSDAY("매주 목요일"),
+    FRIDAY("매주 금요일"),
+    SATURDAY("매주 토요일"),
+    SUNDAY("매주 일요일");
+
+    private final String value;
+
+    Day(String value) {
+        this.value = value;
+    }
+
+
+    public String getValue() {
+        return value;
+    }
+}
