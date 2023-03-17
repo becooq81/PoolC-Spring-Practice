60f354a8703387633b6f0dc058ab5b23048c6b05
jinny-codes <becooq81@gmail.com>
2023-03-17 13:03:11 +0900

    feat(UserRepository): added extension of interfaces to facilitate pagination

--- src/main/java/com/poolc/springproject/poolcreborn/repository/UserRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/UserRepository.java
@@ -1,18 +1,21 @@
 package com.poolc.springproject.poolcreborn.repository;
 
 import com.poolc.springproject.poolcreborn.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, PagingAndSortingRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
-
     List<User> findAll();
 }
