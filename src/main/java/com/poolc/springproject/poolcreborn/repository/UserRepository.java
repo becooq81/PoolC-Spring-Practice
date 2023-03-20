44e5dc7a2aafd71c6a60068df33dcecc84fcceb4
becooq81 <becooq81@gmail.com>
2023-03-20 14:55:03 +0900

    feat: added extension for search api

--- src/main/java/com/poolc/springproject/poolcreborn/repository/UserRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.poolc.springproject.poolcreborn.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>, PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, PagingAndSortingRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
