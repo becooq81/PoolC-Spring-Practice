0109e77a11382fa4caa89979586f32e432f76e88
becooq81 <becooq81@gmail.com>
2023-03-20 15:34:13 +0900

    feat: added UserSpecification class

--- src/main/java/com/poolc/springproject/poolcreborn/util/UserSpecification.java
+++ src/main/java/com/poolc/springproject/poolcreborn/util/UserSpecification.java
@@ -1,2 +1,38 @@
-package com.poolc.springproject.poolcreborn.util;public class UserSpecification {
+package com.poolc.springproject.poolcreborn.util;
+
+import com.poolc.springproject.poolcreborn.model.SearchCriteria;
+import com.poolc.springproject.poolcreborn.model.User;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class UserSpecification implements Specification<User> {
+
+    private SearchCriteria criteria;
+
+    @Override
+    public Predicate toPredicate
+            (Root<User> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+
+        if (criteria.getOperation().equalsIgnoreCase(">")) {
+            return builder.greaterThanOrEqualTo(
+                    root.<String> get(criteria.getKey()), criteria.getValue().toString());
+        }
+        else if (criteria.getOperation().equalsIgnoreCase("<")) {
+            return builder.lessThanOrEqualTo(
+                    root.<String> get(criteria.getKey()), criteria.getValue().toString());
+        }
+        else if (criteria.getOperation().equalsIgnoreCase(":")) {
+            if (root.get(criteria.getKey()).getJavaType() == String.class) {
+                return builder.like(
+                        root.<String>get(criteria.getKey()), "%" + criteria.getValue() + "%");
+            } else {
+                return builder.equal(root.get(criteria.getKey()), criteria.getValue());
+            }
+        }
+        return null;
+    }
 }
