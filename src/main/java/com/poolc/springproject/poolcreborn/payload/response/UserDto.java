7fd5b8006b5de370ad74d29bb6dc55a95bc23e93
becooq81 <becooq81@gmail.com>
2023-03-21 20:40:51 +0900

    feat: added userdto

--- src/main/java/com/poolc/springproject/poolcreborn/payload/response/UserDto.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/response/UserDto.java
@@ -1,4 +1,12 @@
 package com.poolc.springproject.poolcreborn.payload.response;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public class UserDto {
+    private String name;
+    private String major;
+    private String description;
 }
