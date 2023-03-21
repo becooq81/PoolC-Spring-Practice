996491ad18d0ef1e7b6fa7963432793b0283554a
becooq81 <becooq81@gmail.com>
2023-03-21 17:46:14 +0900

    feat: added UserMapper

--- src/main/java/com/poolc/springproject/poolcreborn/util/UserMapper.java
+++ src/main/java/com/poolc/springproject/poolcreborn/util/UserMapper.java
@@ -1,2 +1,18 @@
-package com.poolc.springproject.poolcreborn.util;public interface UserMapper {
+package com.poolc.springproject.poolcreborn.util;
+
+import com.poolc.springproject.poolcreborn.model.user.User;
+import com.poolc.springproject.poolcreborn.payload.request.user.SignupRequest;
+import com.poolc.springproject.poolcreborn.payload.request.user.UserUpdateRequest;
+import org.mapstruct.*;
+
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @BeanMapping(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateUserInfoFromRequest(UserUpdateRequest userUpdateRequest, @MappingTarget User user);
+
+    @BeanMapping(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void buildUserFromRequest(SignupRequest signupRequest, @MappingTarget User user);
+
 }
