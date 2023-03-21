ff0f22137c84b92c65cdb61eb48a87f2082954ea
becooq81 <becooq81@gmail.com>
2023-03-21 17:46:04 +0900

    feat: added ActivityMapper

--- src/main/java/com/poolc/springproject/poolcreborn/util/ActivityMapper.java
+++ src/main/java/com/poolc/springproject/poolcreborn/util/ActivityMapper.java
@@ -1,2 +1,17 @@
-package com.poolc.springproject.poolcreborn.util;public class ActivityMapper {
+package com.poolc.springproject.poolcreborn.util;
+
+import com.poolc.springproject.poolcreborn.model.activity.Activity;
+import com.poolc.springproject.poolcreborn.payload.request.activity.ActivityRequest;
+import com.poolc.springproject.poolcreborn.payload.request.activity.ActivityUpdateRequest;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface ActivityMapper {
+
+    @BeanMapping(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void buildActivityFromRequest(ActivityRequest activityRequest, @MappingTarget Activity activity);
+
+    @BeanMapping(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateActivityInfoFromRequest(ActivityUpdateRequest activityUpdateRequest, @MappingTarget Activity activity);
+
 }
