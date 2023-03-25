b54ab54b708584d6acf114d475de016e943088de
becooq81 <becooq81@gmail.com>
2023-03-25 15:32:22 +0900

    feat: added search category enum

--- src/main/java/com/poolc/springproject/poolcreborn/model/search/SearchCategory.java
+++ src/main/java/com/poolc/springproject/poolcreborn/model/search/SearchCategory.java
@@ -1,2 +1,18 @@
-package com.poolc.springproject.poolcreborn.model.search;public enum SearchCategory {
+package com.poolc.springproject.poolcreborn.model.search;
+
+import com.poolc.springproject.poolcreborn.payload.request.search.SearchRequest;
+
+public enum SearchCategory {
+    USERNAME("username"), NAME("name"), MAJOR("major"), ISCLUBMEMBER("isClubMember"), ISADMIN("isAdmin");
+
+    private final String value;
+
+    SearchCategory(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
 }
