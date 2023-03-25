b95feb0facc439b64f8a240cccfed68428ff2b3e
becooq81 <becooq81@gmail.com>
2023-03-25 14:00:29 +0900

    refactor(SearchOperation): refactored getSearchOperation method to ensure maintainability

--- src/main/java/com/poolc/springproject/poolcreborn/model/search/SearchOperation.java
+++ src/main/java/com/poolc/springproject/poolcreborn/model/search/SearchOperation.java
@@ -1,41 +1,28 @@
 package com.poolc.springproject.poolcreborn.model.search;
 
 public enum SearchOperation {
-    CONTAINS, DOES_NOT_CONTAIN, EQUAL, NOT_EQUAL, BEGINS_WITH,
-    DOES_NOT_BEGIN_WITH, ENDS_WITH, DOES_NOT_END_WITH,
-    NUL, NOT_NULL, GREATER_THAN, GREATER_THAN_EQUAL, LESS_THAN,
-    LESS_THAN_EQUAL, ANY, ALL;
-    public static final String[] SIMPLE_OPERATION_SET = {
-            "cn", "nc", "eq", "ne", "bw", "bn", "ew",
-            "en", "nu", "nn", "gt", "ge", "lt", "le" };
-
-    public static SearchOperation getDataOption(final String dataOption){
-        switch(dataOption){
+    CONTAINS("cn"), DOES_NOT_CONTAIN("nc"), EQUAL("eq"), NOT_EQUAL("ne"), BEGINS_WITH("bw"),
+    DOES_NOT_BEGIN_WITH("bn"), ENDS_WITH("ew"), DOES_NOT_END_WITH("en"),
+    NULL("nu"), NOT_NULL("nn"), GREATER_THAN("gt"), GREATER_THAN_EQUAL("ge"), LESS_THAN("lt"),
+    LESS_THAN_EQUAL("le"), ANY("any"), ALL("all");
+    private final String value;
+    public static SearchOperation getDataOption(final String dataOption) {
+        switch (dataOption) {
             case "all": return ALL;
             case "any": return ANY;
             default: return null;
         }
     }
+    SearchOperation(String value) {
+        this.value = value;
+    }
 
-    public static SearchOperation getSimpleOperation(
-            final String input) {
-        switch (input){
-            case "cn": return CONTAINS;
-            case "nc": return DOES_NOT_CONTAIN;
-            case "eq": return EQUAL;
-            case "ne": return NOT_EQUAL;
-            case "bw": return BEGINS_WITH;
-            case "bn": return DOES_NOT_BEGIN_WITH;
-            case "ew": return ENDS_WITH;
-            case "en": return DOES_NOT_END_WITH;
-            case "nu": return NUL;
-            case "nn": return NOT_NULL;
-            case "gt": return GREATER_THAN;
-            case "ge": return GREATER_THAN_EQUAL;
-            case "lt": return LESS_THAN;
-            case "le": return LESS_THAN_EQUAL;
-
-            default: return null;
+    public static SearchOperation getSearchOperation(final String input) {
+        for (SearchOperation operation : values()) {
+            if (input.equals(operation.value)) {
+                return operation;
+            }
         }
+        return null;
     }
 }
