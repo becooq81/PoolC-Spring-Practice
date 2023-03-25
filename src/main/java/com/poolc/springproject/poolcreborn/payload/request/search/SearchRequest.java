22306181a5dfde71056062fc676e2d75d2bd2eb1
becooq81 <becooq81@gmail.com>
2023-03-25 15:32:32 +0900

    feat: added search request dto

--- src/main/java/com/poolc/springproject/poolcreborn/payload/request/search/SearchRequest.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/request/search/SearchRequest.java
@@ -1,2 +1,25 @@
-package com.poolc.springproject.poolcreborn.payload.request.search;public class SearchRequest {
+package com.poolc.springproject.poolcreborn.payload.request.search;
+
+import com.poolc.springproject.poolcreborn.model.search.SearchCategory;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@ToString
+@Builder(toBuilder = true)
+public class SearchRequest {
+    @NotEmpty
+    private SearchCategory searchCategory;
+    @NotEmpty
+    private String keyword;
+
+    public SearchRequest(SearchCategory searchCategory, String keyword) {
+        this.searchCategory = searchCategory;
+        this.keyword = keyword;
+    }
+
+    public SearchRequest() {}
 }
