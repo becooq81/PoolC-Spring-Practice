d0eaf628dfc1f6c1702c992275981f2e73a67bd8
becooq81 <becooq81@gmail.com>
2023-03-28 01:10:56 +0900

    feat: added method to find whether specific participation exists

--- src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
@@ -1,9 +1,15 @@
 package com.poolc.springproject.poolcreborn.repository;
 
+import com.poolc.springproject.poolcreborn.model.activity.Activity;
 import com.poolc.springproject.poolcreborn.model.participation.Participation;
+import com.poolc.springproject.poolcreborn.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+
+    Boolean existsByActivityAndUser(User user, Activity activity);
 }
