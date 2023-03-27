848db6314a098410793ad73dff8bf6c72b2509fb
becooq81 <becooq81@gmail.com>
2023-03-27 17:28:07 +0900

    feat(ParticipationRequest): added constructor & validation

--- src/main/java/com/poolc/springproject/poolcreborn/payload/request/participation/ParticipationRequest.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/request/participation/ParticipationRequest.java
@@ -4,9 +4,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @ToString
 @Builder(toBuilder = true)
 public class ParticipationRequest {
+    @NotBlank
     private String activity;
+
+    public ParticipationRequest(String activity) {
+        this.activity = activity;
+    }
+    public ParticipationRequest() {}
 }
