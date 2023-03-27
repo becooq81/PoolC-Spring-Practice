d01071c61b7319ef820fa77880e6237b32f47d0a
becooq81 <becooq81@gmail.com>
2023-03-27 17:25:12 +0900

    fix(ParticipationRepository): fixed type of id

--- src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
@@ -1,9 +1,8 @@
 package com.poolc.springproject.poolcreborn.repository;
 
 import com.poolc.springproject.poolcreborn.model.participation.Participation;
-import com.poolc.springproject.poolcreborn.model.participation.ParticipationId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ParticipationRepository extends JpaRepository<Participation, ParticipationId> {
+public interface ParticipationRepository extends JpaRepository<Participation, Long> {
 
 }
