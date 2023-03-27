cd6103060de5240505ee5c273362b3eab37b0260
becooq81 <becooq81@gmail.com>
2023-03-27 18:17:13 +0900

    feat: added activity dto

--- src/main/java/com/poolc/springproject/poolcreborn/payload/response/activity/ActivityDto.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/response/activity/ActivityDto.java
@@ -1,2 +1,22 @@
-package com.poolc.springproject.poolcreborn.payload.response.activity;public class ActivityDto {
+package com.poolc.springproject.poolcreborn.payload.response.activity;
+
+import com.poolc.springproject.poolcreborn.payload.response.user.SimpleUserDto;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Setter
+public class ActivityDto {
+    private String lead;
+    private LocalDate startDate;
+    private int hours;
+    private int capacity;
+    private List<String> tags;
+    private String plan;
+    private Set<SimpleUserDto> participants;
 }
