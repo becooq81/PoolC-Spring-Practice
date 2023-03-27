cab463b1cd8d81596d1cb292b25e34e9daaf742f
becooq81 <becooq81@gmail.com>
2023-03-27 18:49:18 +0900

    feat(ParticipationService): added participation save method

--- src/main/java/com/poolc/springproject/poolcreborn/service/ParticipationService.java
+++ src/main/java/com/poolc/springproject/poolcreborn/service/ParticipationService.java
@@ -1,8 +1,12 @@
 package com.poolc.springproject.poolcreborn.service;
 
 import com.poolc.springproject.poolcreborn.model.activity.Activity;
+import com.poolc.springproject.poolcreborn.model.participation.Participation;
+import com.poolc.springproject.poolcreborn.payload.request.participation.ParticipationRequest;
 import com.poolc.springproject.poolcreborn.payload.response.user.SimpleUserDto;
+import com.poolc.springproject.poolcreborn.repository.ActivityRepository;
 import com.poolc.springproject.poolcreborn.repository.ParticipationRepository;
+import com.poolc.springproject.poolcreborn.repository.UserRepository;
 import com.poolc.springproject.poolcreborn.util.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,10 +20,22 @@ import java.util.stream.Collectors;
 public class ParticipationService {
     private final ParticipationRepository participationRepository;
     private final UserMapper userMapper;
+    private final UserRepository userRepository;
+    private final ActivityRepository activityRepository;
 
     public Set<SimpleUserDto> getParticipants(Activity activity) {
         return activity.getParticipants().stream()
                 .map(p -> userMapper.buildSimpleUserDtoFromUser(p))
                 .collect(Collectors.toSet());
+    }
+
+    public void saveParticipation(String username, ParticipationRequest request) {
+        Participation participation = new Participation();
+        participation.setUser(userRepository.findByUsername(username).get());
+
+        Activity activity = activityRepository.findByTitle(request.getActivity()).get();
+        participation.setActivity(activity);
+
+        participationRepository.save(participation);
     }
 }
