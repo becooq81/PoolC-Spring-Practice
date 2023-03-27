b3de4ee2ae83de94a696ddf6e0f4cb69f9e6b082
becooq81 <becooq81@gmail.com>
2023-03-27 18:21:53 +0900

    feat(CustomMapper): added method that builds activity dto from activity

--- src/main/java/com/poolc/springproject/poolcreborn/util/CustomMapper.java
+++ src/main/java/com/poolc/springproject/poolcreborn/util/CustomMapper.java
@@ -1,2 +1,29 @@
-package com.poolc.springproject.poolcreborn.util;public class CustomMapper {
+package com.poolc.springproject.poolcreborn.util;
+
+import com.poolc.springproject.poolcreborn.model.activity.Activity;
+import com.poolc.springproject.poolcreborn.payload.response.activity.ActivityDto;
+import com.poolc.springproject.poolcreborn.service.ParticipationService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CustomMapper {
+
+    private final ParticipationService participationService;
+
+    ActivityDto buildActivityDtoFromActivity(Activity activity) {
+        if (activity == null) {
+            return null;
+        }
+        ActivityDto activityDto = new ActivityDto();
+
+        activityDto.setLead(activity.getUser().getUsername());
+        activityDto.setStartDate(activity.getStartDate());
+        activityDto.setHours(activity.getHours());
+        activityDto.setCapacity(activity.getCapacity());
+        activityDto.setTags(activity.getTags());
+        activityDto.setPlan(activity.getPlan());
+        activityDto.setParticipants(participationService.getParticipants(activity));
+
+        return activityDto;
+    }
 }
