e395be2eafc4c172cb7e49f60f13629660efe87e
becooq81 <becooq81@gmail.com>
2023-03-28 01:12:24 +0900

    style: removed unnecessary lines

--- src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
@@ -6,8 +6,6 @@ import com.poolc.springproject.poolcreborn.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
 
--- src/main/java/com/poolc/springproject/poolcreborn/repository/RequestedParticipationRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/RequestedParticipationRepository.java
@@ -1,14 +1,10 @@
 package com.poolc.springproject.poolcreborn.repository;
 
-import com.poolc.springproject.poolcreborn.model.activity.Activity;
-import com.poolc.springproject.poolcreborn.model.participation.Participation;
 import com.poolc.springproject.poolcreborn.model.participation.RequestedParticipation;
-import com.poolc.springproject.poolcreborn.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface RequestedParticipationRepository extends JpaRepository<RequestedParticipation, Long> {
