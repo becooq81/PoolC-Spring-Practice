c711ddf808fa91369f600537adaa98347b1e3df0
becooq81 <becooq81@gmail.com>
2023-03-27 21:43:08 +0900

    feat: added Participation Request payload

--- src/main/java/com/poolc/springproject/poolcreborn/payload/request/participation/ParticipationRequest.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/request/participation/ParticipationRequest.java
@@ -1,2 +1,16 @@
-package com.poolc.springproject.poolcreborn.payload.request.participation;public class ParticipationRequest {
+package com.poolc.springproject.poolcreborn.payload.request.participation;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class ParticipationRequest {
+
+    private boolean isApproved;
+
 }
