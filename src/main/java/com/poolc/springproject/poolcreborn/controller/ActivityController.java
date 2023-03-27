f41f774bae972b491ce75c01e85692719e373ef2
becooq81 <becooq81@gmail.com>
2023-03-27 18:27:14 +0900

    feat(ActivityController): added method to view specific activity

--- src/main/java/com/poolc/springproject/poolcreborn/controller/ActivityController.java
+++ src/main/java/com/poolc/springproject/poolcreborn/controller/ActivityController.java
@@ -3,8 +3,13 @@ package com.poolc.springproject.poolcreborn.controller;
 import com.poolc.springproject.poolcreborn.model.activity.Activity;
 import com.poolc.springproject.poolcreborn.payload.request.activity.ActivityRequest;
 import com.poolc.springproject.poolcreborn.payload.request.activity.ActivityUpdateRequest;
+import com.poolc.springproject.poolcreborn.payload.request.participation.ParticipationRequest;
+import com.poolc.springproject.poolcreborn.payload.request.participation.RestrictedParticipationRequest;
+import com.poolc.springproject.poolcreborn.payload.response.activity.ActivityDto;
 import com.poolc.springproject.poolcreborn.repository.ActivityRepository;
+import com.poolc.springproject.poolcreborn.repository.ParticipationRepository;
 import com.poolc.springproject.poolcreborn.service.ActivityService;
+import com.poolc.springproject.poolcreborn.util.CustomMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+
+import java.util.List;
 
 import static com.poolc.springproject.poolcreborn.security.SecurityUtil.getLoginUsername;
 
@@ -21,12 +28,21 @@ import static com.poolc.springproject.poolcreborn.security.SecurityUtil.getLogin
 public class ActivityController {
     private final ActivityRepository activityRepository;
     private final ActivityService activityService;
+    private final ParticipationRepository participationRepository;
+    private final CustomMapper customMapper;
 
     @PostMapping("/new")
     public ResponseEntity<?> registerActivity(@RequestBody @Valid ActivityRequest activityRequest) {
         String username = getLoginUsername();
         activityService.saveActivity(activityRequest, username);
         return ResponseEntity.ok(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ActivityDto> viewActivity(@PathVariable("id") @Min(1) Long currentActivityId) {
+        String username = getLoginUsername();
+        Activity activity = activityRepository.findById(currentActivityId).get();
+        return new ResponseEntity<>(customMapper.buildActivityDtoFromActivity(activity), HttpStatus.OK);
     }
 
     @PatchMapping("/{id}/edit")
