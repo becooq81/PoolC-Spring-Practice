c104f2aebcae55da7b9948d0be905a449d7905c8
becooq81 <becooq81@gmail.com>
2023-03-27 17:04:05 +0900

    feat: added participation repository

---
+++ src/main/java/com/poolc/springproject/poolcreborn/model/activity/Activity.java
@@ -0,0 +1,69 @@
+package com.poolc.springproject.poolcreborn.model.activity;
+
+import com.poolc.springproject.poolcreborn.model.participation.Participation;
+import com.poolc.springproject.poolcreborn.model.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Activity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @NotNull
+    private LocalDate startDate;
+
+    private String semester;
+
+    @Enumerated(EnumType.STRING)
+    private ActivityType activityType;
+
+    @NotNull
+    @Max(100)
+    private int capacity;
+
+    @NotBlank
+    @Enumerated
+    private Day day;
+
+    @NotNull
+    @Max(10)
+    private int hours;
+    @ElementCollection(targetClass=String.class, fetch = FetchType.EAGER)
+    private List<String> tags;
+
+    @NotBlank
+    private String plan;
+
+    @OneToMany(mappedBy = "activity")
+    private List<Participation> participationList = new ArrayList<>();
+
+    public Activity(String title, LocalDate startDate, ActivityType activityType, int capacity, Day day, int hours, List<String> tags) {
+        this.title = title;
+        this.startDate = startDate;
+        this.activityType = activityType;
+        this.capacity = capacity;
+        this.day = day;
+        this.hours = hours;
+        this.tags = tags;
+    }
+
+    public Activity() {}
+}
--- src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
@@ -1,2 +1,9 @@
-package com.poolc.springproject.poolcreborn.repository;public class ParticipationRepository {
+package com.poolc.springproject.poolcreborn.repository;
+
+import com.poolc.springproject.poolcreborn.model.participation.Participation;
+import com.poolc.springproject.poolcreborn.model.participation.ParticipationId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipationRepository extends JpaRepository<Participation, ParticipationId> {
+
 }
