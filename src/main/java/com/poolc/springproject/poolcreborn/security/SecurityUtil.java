7e5dfd7ffd570386aa1534a9e62e6aa5b326c24f
becooq81 <becooq81@gmail.com>
2023-03-28 01:35:00 +0900

    style: removed unnecessary lines

--- src/main/java/com/poolc/springproject/poolcreborn/security/SecurityUtil.java
+++ src/main/java/com/poolc/springproject/poolcreborn/security/SecurityUtil.java
@@ -2,7 +2,6 @@ package com.poolc.springproject.poolcreborn.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public class SecurityUtil {
     public static String getLoginUsername(){
