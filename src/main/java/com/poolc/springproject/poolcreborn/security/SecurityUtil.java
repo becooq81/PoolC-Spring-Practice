b05929b2cf570e447419facb43314227aef354eb
becooq81 <becooq81@gmail.com>
2023-03-28 01:34:26 +0900

    fix: modified code to avoid casting

--- src/main/java/com/poolc/springproject/poolcreborn/security/SecurityUtil.java
+++ src/main/java/com/poolc/springproject/poolcreborn/security/SecurityUtil.java
@@ -1,11 +1,13 @@
 package com.poolc.springproject.poolcreborn.security;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class SecurityUtil {
     public static String getLoginUsername(){
-        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return user.getUsername();
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+        String username = loggedInUser.getName();
+        return username;
     }
 }
