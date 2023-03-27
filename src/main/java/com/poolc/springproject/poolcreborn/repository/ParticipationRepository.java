ae9d2c0c3d2550758f165d255f08826ed42c0d72
becooq81 <becooq81@gmail.com>
2023-03-28 00:30:15 +0900

    fix: added repository annotation

--- src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
@@ -2,6 +2,8 @@ package com.poolc.springproject.poolcreborn.repository;
 
 import com.poolc.springproject.poolcreborn.model.participation.Participation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
 }
