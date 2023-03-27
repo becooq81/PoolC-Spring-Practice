e390b96214ff6b7c23e5423e5971be993ec848dc
becooq81 <becooq81@gmail.com>
2023-03-27 18:17:39 +0900

    feat: added participation service

--- src/main/java/com/poolc/springproject/poolcreborn/service/ParticipationService.java
+++ src/main/java/com/poolc/springproject/poolcreborn/service/ParticipationService.java
@@ -1,2 +1,24 @@
-package com.poolc.springproject.poolcreborn.service;public class ParticipationService {
+package com.poolc.springproject.poolcreborn.service;
+
+import com.poolc.springproject.poolcreborn.model.activity.Activity;
+import com.poolc.springproject.poolcreborn.payload.response.user.SimpleUserDto;
+import com.poolc.springproject.poolcreborn.repository.ParticipationRepository;
+import com.poolc.springproject.poolcreborn.util.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ParticipationService {
+    private final ParticipationRepository participationRepository;
+    private final UserMapper userMapper;
+
+    public List<SimpleUserDto> getParticipants(Activity activity) {
+        activity.getParticipants().stream()
+                .map(p -> userMapper.buildSimpleUserDtoFromUser(p))
+                .collect(Collectors.toList());
+    }
 }
