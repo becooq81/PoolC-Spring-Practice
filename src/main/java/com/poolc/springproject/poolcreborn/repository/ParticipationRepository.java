09ce4f545c174f80783583604228c27ca96829bd
becooq81 <becooq81@gmail.com>
2023-03-27 22:05:40 +0900

    style: removed unnecessary lines

--- src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
@@ -4,5 +4,4 @@ import com.poolc.springproject.poolcreborn.model.participation.Participation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
-
 }
