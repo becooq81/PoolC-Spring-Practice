418a66abbac96885f9a3f4a6fbec7a9d9b87fe37
becooq81 <becooq81@gmail.com>
2023-03-27 21:43:23 +0900

    feat: added RequestedParticipationDto

--- src/main/java/com/poolc/springproject/poolcreborn/payload/response/RequestedParticipationDto.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/response/RequestedParticipationDto.java
@@ -1,2 +1,17 @@
-package com.poolc.springproject.poolcreborn.payload.response;public class RequestedParticipationDto {
+package com.poolc.springproject.poolcreborn.payload.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class RequestedParticipationDto {
+
+    private String username;
+    private String activityTitle;
+    private String major;
+
 }
