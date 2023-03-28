425b1bedd66a2714049f67350de72e812ca905a5
becooq81 <becooq81@gmail.com>
2023-03-28 15:48:08 +0900

    feat: added constructors

--- src/main/java/com/poolc/springproject/poolcreborn/payload/response/RequestedParticipationDto.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/response/RequestedParticipationDto.java
@@ -2,14 +2,22 @@ package com.poolc.springproject.poolcreborn.payload.response;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
-@NoArgsConstructor
 public class RequestedParticipationDto {
 
     private String username;
     private String activityTitle;
     private String major;
+
+    public RequestedParticipationDto(String username, String activityTitle, String major) {
+        this.username = username;
+        this.activityTitle = activityTitle;
+        this.major = major;
+    }
+
+    public RequestedParticipationDto() {}
 
 }
