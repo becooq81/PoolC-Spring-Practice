800ba3d0e7e3ea8421dadcc805cb6403ede26fbc
becooq81 <becooq81@gmail.com>
2023-03-28 11:14:13 +0900

    feat: added UserHoursDto

--- src/main/java/com/poolc/springproject/poolcreborn/payload/response/user/UserHoursDto.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/response/user/UserHoursDto.java
@@ -1,4 +1,20 @@
 package com.poolc.springproject.poolcreborn.payload.response.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserHoursDto {
+    private String username;
+    private String name;
+    private String major;
+    private int studentId;
+    private int totalHours;
+    private boolean isAdmin;
+
 }
