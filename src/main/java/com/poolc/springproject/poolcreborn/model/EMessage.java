5306d1ac15e7a1216cc4738eab7132c4d379617b
becooq81 <becooq81@gmail.com>
2023-03-28 14:51:52 +0900

    refactor: translated error messages to English

--- src/main/java/com/poolc/springproject/poolcreborn/model/EMessage.java
+++ src/main/java/com/poolc/springproject/poolcreborn/model/EMessage.java
@@ -1,20 +1,19 @@
 package com.poolc.springproject.poolcreborn.model;
 
 public enum EMessage {
-    USED_EMAIL("이미 사용중인 이메일입니다."),
-    USED_USERNAME("이미 사용중인 아이디입니다."),
+    USED_EMAIL("This message is already in use."),
+    USED_USERNAME("This username is already in use."),
 
-    ALREADY_SIGNED_UP("이미 신청한 활동입니다."),
-    SELF_SIGNUP_DENIED("본인의 활동은 신청할 수 없습니다."),
-    UPDATE_ACTIVITY_ACCESS_DENIED("본인이 개설한 활동만 수정할 수 있습니다."),
-    APPROVAL_ACCESS_DENIED("본인이 개설한 활동만 신청 요청자를 승인할 수 있습니다."),
-
-    SUCCESSFUL_ROLE_ADD("성공적으로 권한을 부여했습니다."),
-    SUCCESSFUL_UPDATE("성공적으로 수정했습니다."),
-    SUCCESSFUL_SIGNUP("성공적으로 신청되었습니다."),
-    SUCCESSFUL_SIGNUP_REQUEST("성공적으로 신청을 요청했습니다."),
-    SUCCESSFUL_REQUEST_APPROVAL("성공적으로 요청을 승인했습니다."),
-    SUCCESSFUL_DELETE_USER("성공적으로 탈퇴했습니다.");
+    ALREADY_SIGNED_UP("You already signed up for this activity."),
+    SELF_SIGNUP_DENIED("You cannot sign up for your own activity."),
+    UPDATE_ACTIVITY_ACCESS_DENIED("You can only update your own activity."),
+    APPROVAL_ACCESS_DENIED("You can only approve requests for your own activity."),
+    SUCCESSFUL_ROLE_ADD("You have successfully added roles."),
+    SUCCESSFUL_UPDATE("You have successfully updated your activity."),
+    SUCCESSFUL_SIGNUP("You have successfully signed up."),
+    SUCCESSFUL_SIGNUP_REQUEST("You have successfully requested signup."),
+    SUCCESSFUL_REQUEST_APPROVAL("You have successfully approved requests."),
+    SUCCESSFUL_DELETE_USER("You have successfully deleted your account.");
 
     private final String message;
     EMessage(String message) {
