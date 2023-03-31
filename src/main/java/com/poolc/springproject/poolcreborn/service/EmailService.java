edbc0187dc1d2df14253b377d69e09ef99f72ee2
becooq81 <becooq81@gmail.com>
2023-03-31 19:53:53 +0900

    feat: added email service

--- src/main/java/com/poolc/springproject/poolcreborn/service/EmailService.java
+++ src/main/java/com/poolc/springproject/poolcreborn/service/EmailService.java
@@ -1,2 +1,70 @@
-package com.poolc.springproject.poolcreborn.service;public class EmailService {
+package com.poolc.springproject.poolcreborn.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine templateEngine;
+    private String authNum;
+
+    public void createCode() {
+        Random random = new Random();
+        StringBuffer key = new StringBuffer();
+
+        for(int i=0;i<8;i++) {
+            int index = random.nextInt(3);
+
+            switch (index) {
+                case 0 :
+                    key.append((char) ((int)random.nextInt(26) + 97));
+                    break;
+                case 1:
+                    key.append((char) ((int)random.nextInt(26) + 65));
+                    break;
+                case 2:
+                    key.append(random.nextInt(9));
+                    break;
+            }
+        }
+        authNum = key.toString();
+    }
+
+    public MimeMessage createEmailForm(String email) throws MessagingException, UnsupportedEncodingException {
+        createCode();
+        String setFrom = "becooq81@gmail.com";
+        String toEmail = email;
+        String title = "PoolC Verification Code";
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        message.addRecipients(MimeMessage.RecipientType.TO, email);
+        message.setSubject(title);
+        message.setFrom(setFrom);
+        message.setText(setContext(authNum), "utf-8", "html");
+
+        return message;
+    }
+
+    public String sendEmail(String toEmail) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage emailForm = createEmailForm(toEmail);
+        javaMailSender.send(emailForm);
+
+        return authNum;
+    }
+
+    public String setContext(String code) {
+        Context context = new Context();
+        context.setVariable("code", code);
+        return templateEngine.process("mail", context);
+    }
 }
