68dc5b2d3221a472cde13199ceb561a8406f8d03
becooq81 <becooq81@gmail.com>
2023-03-31 19:51:56 +0900

    feat(AuthController): added method to return verification code

--- src/main/java/com/poolc/springproject/poolcreborn/controller/AuthController.java
+++ src/main/java/com/poolc/springproject/poolcreborn/controller/AuthController.java
@@ -1,17 +1,23 @@
 package com.poolc.springproject.poolcreborn.controller;
 
+import com.poolc.springproject.poolcreborn.payload.request.EmailAuthRequestDto;
 import com.poolc.springproject.poolcreborn.payload.request.user.SignupRequest;
 import com.poolc.springproject.poolcreborn.payload.request.user.LoginRequest;
 import com.poolc.springproject.poolcreborn.repository.UserRepository;
+import com.poolc.springproject.poolcreborn.service.EmailService;
 import com.poolc.springproject.poolcreborn.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import java.io.UnsupportedEncodingException;
+
+import static com.poolc.springproject.poolcreborn.security.SecurityUtil.getLoginUsername;
 import static com.poolc.springproject.poolcreborn.util.Message.*;
 
 @RestController
@@ -19,10 +25,21 @@ import static com.poolc.springproject.poolcreborn.util.Message.*;
 public class AuthController {
     private final UserRepository userRepository;
     private final UserService userService;
+    private final EmailService emailService;
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(HttpServletRequest request, @Valid @RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok(userService.authenticateUser(loginRequest));
+    }
+
+    @PostMapping("/login/confirm/mail")
+    public String cofirmMail(@RequestBody @Valid EmailAuthRequestDto emailDto) throws MessagingException, UnsupportedEncodingException {
+        String username = getLoginUsername();
+        String email = userRepository.findByUsername(username).get().getEmail();
+        if (email.equals(emailDto.getEmail())) {
+            return emailService.sendEmail(emailDto.getEmail());
+        }
+        return WRONG_EMAIL;
     }
 
     @PostMapping("/signup")
--- src/main/java/com/poolc/springproject/poolcreborn/model/EmailMessage.java
+++
@@ -1,2 +0,0 @@
-package com.poolc.springproject.poolcreborn.model.user;public class EmailMessage {
-}
