18a94151b707d7aec0770219d9c5c794f46ba65e
becooq81 <becooq81@gmail.com>
2023-03-31 19:52:54 +0900

    feat: added email request dto

--- src/main/java/com/poolc/springproject/poolcreborn/payload/request/EmailAuthRequestDto.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/request/EmailAuthRequestDto.java
@@ -1,2 +1,13 @@
-package com.poolc.springproject.poolcreborn.model;public class EmailAuthRequestDto {
+package com.poolc.springproject.poolcreborn.payload.request;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@ToString
+public class EmailAuthRequestDto {
+    @NotEmpty(message = "Enter your email.")
+    public String email;
 }
