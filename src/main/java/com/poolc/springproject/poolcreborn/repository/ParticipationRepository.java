babec696373a420cd4f8e628c48ce5f4101e4b23
becooq81 <becooq81@gmail.com>
2023-04-05 19:19:45 +0900

    refactor: refactored method

--- src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
@@ -7,13 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
-
     Boolean existsByActivityAndUser(Activity activity, User user);
     List<Participation> findByActivityTitle(String activityTitle);
-    Boolean existsByActivityTitleAndUsername(String activityTitle, String username);
+    Optional<Participation> findByUserAndActivity(User user, Activity activity);
     List<Participation> findByActivityTitleAndIsApproved(String activityTitle, boolean isApproved);
 
 }
