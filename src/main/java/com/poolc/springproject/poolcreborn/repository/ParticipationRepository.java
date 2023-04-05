d73d600b254c73bb5583affa48a735a7402027fe
becooq81 <becooq81@gmail.com>
2023-04-05 18:13:33 +0900

    feat: merged requested participation repository into participation repository

--- src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
+++ src/main/java/com/poolc/springproject/poolcreborn/repository/ParticipationRepository.java
@@ -6,8 +6,14 @@ import com.poolc.springproject.poolcreborn.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
 
     Boolean existsByActivityAndUser(Activity activity, User user);
+    List<Participation> findByActivityTitle(String activityTitle);
+    Boolean existsByActivityTitleAndUsername(String activityTitle, String username);
+    List<Participation> findByActivityTitleAndIsApproved(String activityTitle, boolean isApproved);
+
 }
