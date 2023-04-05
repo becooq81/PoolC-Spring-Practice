95e3c6305cee9e9e9140c641656d6c3966819430
becooq81 <becooq81@gmail.com>
2023-04-05 19:19:33 +0900

    style: reorganized entity

--- src/main/java/com/poolc/springproject/poolcreborn/model/participation/Participation.java
+++ src/main/java/com/poolc/springproject/poolcreborn/model/participation/Participation.java
@@ -15,12 +15,12 @@ public class Participation {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "activity_id")
-    private Activity activity;
-
-    @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "activity_id")
+    private Activity activity;
 
     private boolean isApproved;
 
