000e9b113ef46fff1f3b19d68a84248ca3e59a2f
becooq81 <becooq81@gmail.com>
2023-04-08 01:56:11 +0900

    feat: added messages regarding cancelling participation

--- src/main/java/com/poolc/springproject/poolcreborn/util/Message.java
+++ src/main/java/com/poolc/springproject/poolcreborn/util/Message.java
@@ -7,12 +7,14 @@ public final class Message {
     public static final String WRONG_EMAIL = "Please enter your email correctly.";
     public static final String USER_DOES_NOT_EXIST = "The user does not exist.";
     public static final String SELF_SIGNUP_DENIED = "You cannot sign up for your own activity.";
+    public static final String PARTICIPATION_DELETE_DENIED = "You can only cancel your own participation.";
     public static final String UPDATE_ACTIVITY_ACCESS_DENIED = "You can only update your own activity.";
     public static final String APPROVAL_ACCESS_DENIED = "You can only approve requests for your own activity.";
     public static final String FAIL_SIGNUP_ACTIVITY = "You failed to sign up for this activity.";
     public static final String SUCCESSFUL_SIGNUP_USER = "You have successfully created an account.";
     public static final String SUCCESSFUL_CREATED_ACTIVITY = "You have successfully created an activity";
     public static final String SUCCESSFUL_ROLE_ADD = "You have successfully added roles.";
+    public static final String SUCCESSFUL_DELETE_PARTICIPATION = "You have successfully canceled your participation in this activity.";
     public static final String SUCCESSFUL_UPDATE_ACTIVITY = "You have successfully updated your activity.";
     public static final String SUCCESSFUL_SIGNUP_ACTIVITY = "You have successfully signed up or requested sign-up for this activity.";
     public static final String SUCCESSFUL_REQUEST_APPROVAL = "You have successfully approved requests.";
