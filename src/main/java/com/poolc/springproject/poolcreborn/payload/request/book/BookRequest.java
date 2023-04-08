d506edeacc30ea871d197927f4a6334cbf946642
becooq81 <becooq81@gmail.com>
2023-04-08 16:42:55 +0900

    feat: added book payloads

--- src/main/java/com/poolc/springproject/poolcreborn/payload/request/book/BookRequest.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/request/book/BookRequest.java
@@ -1,2 +1,20 @@
-package com.poolc.springproject.poolcreborn.payload.request.book;public class BookRequest {
+package com.poolc.springproject.poolcreborn.payload.request.book;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+@Builder
+@Getter
+public class BookRequest {
+    @NotEmpty
+    private String title;
+    private Integer isbn;
+    @NotEmpty
+    private String author;
+    private LocalDate published;
+    private String publisher;
+    private Integer category;
 }
--- src/main/java/com/poolc/springproject/poolcreborn/payload/response/book/BookDto.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/response/book/BookDto.java
@@ -1,2 +1,22 @@
-package com.poolc.springproject.poolcreborn.payload.response.book;public class bookDto {
+package com.poolc.springproject.poolcreborn.payload.response.book;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.json.simple.JSONObject;
+
+
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+@Builder
+@Getter
+public class BookDto {
+    @NotEmpty
+    private String title;
+    private Integer isbn;
+    private String author;
+    private LocalDate published;
+    private String publisher;
+    private Integer category;
 }
