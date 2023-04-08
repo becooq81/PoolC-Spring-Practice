d981a27e28464518bfcd9dec60881cee2088182a
becooq81 <becooq81@gmail.com>
2023-04-08 16:04:28 +0900

    feat: added book search request payload

--- src/main/java/com/poolc/springproject/poolcreborn/payload/request/book/BookSearchRequest.java
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/request/book/BookSearchRequest.java
@@ -1,2 +1,20 @@
-package com.poolc.springproject.poolcreborn.payload.request.book;public class BookSearchRequest {
+package com.poolc.springproject.poolcreborn.payload.request.book;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+
+@Builder(toBuilder = true)
+@Getter
+@ToString
+public class BookSearchRequest {
+
+    @NotEmpty
+    private String query;
+    private Integer display;
+    private Integer start;
+    private String sort;
+
 }
