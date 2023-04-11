528b3e21b5796dbf02e8b6235cd2fa0b28d31e17
becooq81 <becooq81@gmail.com>
2023-04-11 15:10:27 +0900

    feat: added book delete request payload

---
+++ src/main/java/com/poolc/springproject/poolcreborn/payload/request/book/BookDeleteRequest.java
@@ -0,0 +1,16 @@
+package com.poolc.springproject.poolcreborn.payload.request.book;
+
+import com.poolc.springproject.poolcreborn.validator.CorrectDeleteMessage;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+@Getter
+@ToString
+@Builder(toBuilder = true)
+public class BookDeleteRequest {
+    @CorrectDeleteMessage
+    @NotEmpty
+    private String message;
+}
